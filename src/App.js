98cc3dff43f22d8aac43106f555442ca3619282a
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-11-15 00:42:03 +0100

    added google font and images

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
       <Navbar />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import {FiBell, FiPlus} from 'react-icons/fi'
+import {IoMdArrowDropdown} from 'react-icons/io'
 
 function Navbar() {
   return (
-    <nav>
+    <nav className='navbar'>
+      {/* Left Navbar */}
       <div className='navLeft'>
-        <img src="../img/gtihub.png" alt=""  />
+        <img src="/src/img/github.svg" alt=""  />
+
+        <div>
+            <input type="text" placeholder='Search or jump to..' />
+        </div>
+      </div>
+
+      {/* Center Navbar */}
+      <div className="navCenter">
+        <h2>Pulls</h2>
+        <h2>issues</h2>
+        <h2>Marketplace</h2>
+        <h2>Explore</h2>
+      </div>
+
+      {/* Right Navbar */}
+      <div className="navRight">
+        <FiBell />
+        <div className="navRightIcons">
+            <FiPlus />
+            <IoMdArrowDropdown />
+        </div>
+        <div className="navRightIcons">
+            <img src="/src/img/tihanapic.png" alt="apple" />
+            <IoMdArrowDropdown />
+        </div>
       </div>
     </nav>
   );
