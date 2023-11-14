2f13c6d87bcb75bff5b4ff72d69057992cf1fc04
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-11-14 14:56:29 +0100

    github ui in react

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      <div className='navLeft'>
+        <img src="../img/gtihub.png" alt=""  />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
